801ef9c776d1c4b21d0f7f7ccb4f8b4e30c7fda6
Adele Kapellusch <akoutia@email.arizona.edu>
2017-05-01 11:47:07 -0700

    Trying to fix plotting moving

--- plotLearning.py
+++ plotLearning.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
 
 
-def prep_data(movAvg):
+def prep_data(movAvg,ratData=None):
     """
     Calculates proportions for the trace.
     """
-    with open('decisions.txt','r') as f:
-        lines = f.readlines()
-    data = [line.split(',') for line in lines][0]
+    if ratData:
+        data = ratData
+    else:
+        with open('decisions.txt','r') as f:
+            lines = f.readlines()
+        data = [line.split(',') for line in lines][0]
     normalize = 0
     sum =0
     proportions = []
@@ -69,7 +73,7 @@ def plot_results(proportions,trialNum,movAvg,alpha,epsilon,discount):
     newYoungY = youngf(newYoungX)
 
     #old rat
-    oldY = rat_data()[1]
+    oldY = movAvg,rat_data()[1]
     oldX = trials[0:len(oldY)]
     oldZ = np.polyfit(oldX,oldY,4)
     oldf = np.poly1d(oldZ)
@@ -98,15 +102,63 @@ def plot_results(proportions,trialNum,movAvg,alpha,epsilon,discount):
     plt.xlabel("Cumulative Count of Trials")
     plt.ylabel("Proportion Correct")
     plt.savefig(figureName)
-    # plt.close()
     plt.show()
+
+
+def plot_avg(proportions,trials,movAvg,alpha,epsilon,discount):
+    trials = range(0, trials, movAvg)
+    figureName = 'LearningCurveMovAvg.png'
+    plt.figure('Learning Curve')
+
+    new_length = 25
+    #young
+    youngY = prep_data(movAvg,rat_data()[0])
+    youngX = range(1,len(youngY)+1)
+    youngPoints = zip(youngX,youngY)
+    youngPoints = sorted(youngPoints, key=lambda point: point[0])
+    youngX1, youngY1 = zip(*youngPoints)
+    new_youngx = np.linspace(min(youngX1), max(youngX1), new_length)
+    new_youngy = interp1d(youngX1, youngY1, kind='cubic')(new_youngx)
+
+    #old
+    oldY = prep_data(movAvg,rat_data()[1])
+    oldX = range(1,len(oldY)+1)
+    oldPoints = zip(oldX, oldY)
+    oldPoints = sorted(oldPoints, key=lambda point: point[0])
+    oldX1, oldY1 = zip(*oldPoints)
+    new_oldx = np.linspace(min(oldX1), max(oldX1), new_length)
+    new_oldy = interp1d(oldX1, oldY1, kind='cubic')(new_oldx)
+
+    #agent
+    x = range(1,len(oldY)+1)
+    y = proportions
+    points = zip(x, y)
+    points = sorted(points, key=lambda point: point[0])
+    x1, y1 = zip(*points)
+    new_x = np.linspace(min(x1), max(x1), new_length)
+    new_y = interp1d(x1, y1, kind='cubic')(new_x)
+
+    agent = plt.plot(new_x, new_y, '-')
+    young = plt.plot(new_youngx, new_youngy, '-')
+    old = plt.plot(new_oldx, new_oldy, '-')
+    plt.setp(agent, linewidth=3, color='purple',
+             label='agent:\nalpha={0}\nepsilon={1}\ngamma={2}'.format(alpha, epsilon, discount))
+    plt.setp(young, linewidth=3, color='green', label='young')
+    plt.setp(old, linewidth=3, color='orange', label='old')
+    plt.title('Learning Curve')
+    plt.xlabel("Cumulative Count of Trials")
+    plt.ylabel("Proportion Correct in " + str(movAvg) + "-trial moving window")
+    plt.savefig(figureName)
+
 
 def plot(alpha,epsilon,discount):
     props,trials = prep_data(1)
     plot_results(props,trials,1,alpha,epsilon,discount)
 
+
+
 def movAvg(alpha,epsilon,discount):
-    props,trials = prep_data(10)
-    plot_results(props,trials,10,alpha,epsilon,discount)
+    props,trials = prep_data(50)
+    plot_avg(props,trials,50,alpha,epsilon,discount)
 
 
