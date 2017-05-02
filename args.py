a7370fe0615fcff0e7d1ed8b350116d9cb78bb41
Adele Kapellusch <akoutia@email.arizona.edu>
2017-05-02 14:40:50 -0700

    Updated arg parsing

--- args.py
+++ args.py
@@ -1,13 +1,11 @@
 import sys
 
 def parseArgs(cl):
-    args = {'r':-1,'e':'greedy0.1','a':'constant0.5','i':15,'d':'constant0.7','p':'polynomial'}
+    args = {'r':-1,'e':'greedy0.1','a':'constant0.5','i':15,'d':'constant0.7','p':'movAvg'}
     for n in range(len(cl)):
         if cl[n] == '-h':
             print ""   #### NEED TO HAVE HELP STATEMENTS
             sys.exit()
-        if cl[n] == '-r': #living reward
-            args['r'] = float(cl[n+1])
         if cl[n] == '-i': #iterations
             args['i'] = int(cl[n+1])
         if cl[n] == '-dConstant': #discount
@@ -34,37 +32,36 @@ def parseArgs(cl):
             args['a'] = 'exponential'
         if cl[n] == '-movAvg':
             args['p'] = 'movAvg'
+        if cl[n] == '-polynomial':
+            args['p'] = 'polynomial'
 
     return args
 
 
 def eps(parameters):
     if parameters['e'] == 'linear':
-        eps = 'Decreasing Linearly'
+        return 'Decreasing Linearly'
     if parameters['e'] == 'exponential':
-        eps = 'Decreasing Exponentially'
+        return 'Decreasing Exponentially'
     if parameters['e'] == 'softmax':
-        eps = 'Softmax'
+        return 'Softmax'
     if parameters['e'] == 'glie':
-        eps = 'GLIE'
+        return 'GLIE'
     else:
-        eps = '{0} {1}'.format(parameters['e'][0:6], parameters['e'][6:])
-    return eps
+        return '{0} {1}'.format(parameters['e'][0:6], parameters['e'][6:])
 
 def alp(parameters):
     if 'constant' in parameters['a']:
-        alp = parameters['a'][8:]
+        return parameters['a'][8:]
     if parameters['a'] == 'linear':
-        alp = 'Decreasing Linearly'
+        return 'Decreasing Linearly'
     if parameters['a'] == 'exponential':
-        alp = 'Decreasing Exponentially'
-    return alp
+        return 'Decreasing Exponentially'
 
 def dis(parameters):
     if parameters['d'] == 'linear':
-        dis = 'Decreasing Linearly'
+        return 'Decreasing Linearly'
     if parameters['d'] == 'quick':
-        dis = 'Decreasing x/(x+1)'
+        return 'Decreasing x/(x+1)'
     if 'constant' in parameters['d']:
-        dis = parameters['d'][8:]
-    return dis
+        return parameters['d'][8:]
